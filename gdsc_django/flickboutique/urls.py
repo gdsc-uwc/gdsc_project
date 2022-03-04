5803fe6f9d7b0d0918cb450d8f26e164e9689deb
Rofhiwa <rofhima13@gmail.com>
2022-03-04 22:33:26 +0200

    Created customer and business signup forms

--- gdsc_django/flickboutique/urls.py
+++ gdsc_django/flickboutique/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'flickboutique'
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("customer-signup", views.customerSignup, name="customerSignup"),
+    path("business-signup", views.businessSignup, name="businessSignup"),
 ]
--- gdsc_django/flickboutique/views.py
+++ gdsc_django/flickboutique/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome!")
+    return render(request, 'flickboutique/layout.html')
+
+def customerSignup(request):
+
+    form = forms.CustomerSignupForm()
+
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'flickboutique/customerSignup.html', context)
+
+def businessSignup(request):
+
+    form = forms.BusinessSignupForm()
+
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'flickboutique/businessSignup.html', context)
